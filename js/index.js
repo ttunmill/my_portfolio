8b10cb4887fbe6946ed7c19c7a2bb878add77548
TtunMill <84768554+ttunmill@users.noreply.github.com>
2024-07-09 20:00:41 +0900

    포트폴리오 제작

--- js/index.js
+++ js/index.js
@@ -20,3 +20,21 @@ wave2.innerHTML = wave2.textContent
     }ms" class="letter">${letter}</span>`;
 })
 .join("");
+
+// 스크롤 게이지 바
+window.addEventListener('scroll', function() {
+  // 게이지바 엘리먼트와 현재 스크롤 위치를 가져옴
+  var gaugeBar = document.querySelector('.scroll_gauge');
+  var scrollPosition = window.scrollY || window.pageYOffset;
+
+  // 전체 페이지 높이와 브라우저 창 높이를 가져옴
+  var pageHeight = document.documentElement.scrollHeight;
+  var windowHeight = window.innerHeight;
+
+  // 현재 스크롤 위치에서 게이지바가 차는 비율을 계산
+  var fillRatio = scrollPosition / (pageHeight - windowHeight);
+  
+   // 계산된 비율로 게이지바 채우기
+  gaugeBar.style.width = (fillRatio * 100) + '%';
+
+});
